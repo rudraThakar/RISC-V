76a133f91cfa996709fa043fafc8c987fc86b5b1
RudraThakar <b23ee1100@iitj.ac.in>
2025-09-05 01:35:58 +0530

    Upadted the Data memory

--- Data_Memory.v
+++ Data_Memory.v
@@ -1,24 +1,26 @@
 
 module Data_Memory(clk,rst,WE,WD,A,RD);
+    input clk, rst, WE;
+    input [31:0] A, WD;
+    output [31:0] RD;
 
-    input clk,rst,WE;
-    input [31:0]A,WD;
-    output [31:0]RD;
+    reg [31:0] data_mem [1023 : 0];
 
-    reg [31:0] mem [1023:0];
+//Logic for right shift is similar to that as in Instructuon memory
 
-    always @ (posedge clk)
-    begin
-        if(WE)
-            mem[A] <= WD;
+    always @(posedge clk)
+        begin
+            if (WE)
+                data_mem[A>>2] <= WD;
+        end
+
+    assign RD = (rst == 1'b1) ? {32{1'b0}} : data_mem[A>>2]
+
+    integer i;
+    inital begin
+        for(i=0; i<1024; i=i+1)
+            data_mem[i] = 32'b0;
     end
-
-    assign RD = (~rst) ? 32'd0 : mem[A];
-
-    initial begin
-        mem[0] = 32'h00000000;
-        //mem[40] = 32'h00000002;
-    end
-
 
 endmodule
+
