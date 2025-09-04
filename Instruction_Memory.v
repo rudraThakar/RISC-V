6048051bca1f8f101d1d13cd5f9227eeac1d0281
RudraThakar <b23ee1100@iitj.ac.in>
2025-09-05 01:36:26 +0530

    Updated Instruction memory

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -5,24 +5,28 @@ module Instruction_Memory(rst,A,RD);
   input [31:0]A;
   output [31:0]RD;
 
-  reg [31:0] mem [1023:0];
-  
-  assign RD = (rst == 1'b0) ? {32{1'b0}} : mem[A[31:2]];
+//4 KB of instruction memory.
+reg [31:0] inst_mem [1023:0]
 
-  initial begin
-    $readmemh("memfile.hex",mem);
-  end
+// A is the PC
+// A = 0000 -> inst_mem[0]
+// A = 0004 -> inst_mem[1]
+// A = 0008 -> inst_mem[2]
+assign RD = (rst = 1'b1) ? {32{1'b1}} : inst_mem[A >> 2]; 
 
+    // Initialize memory contents directly in code
+initial begin
+    
+    mem[0] = 32'h20080005;  // addi $t0, $zero, 5
+    mem[1] = 32'h20090003;  // addi $t1, $zero, 3
+    mem[2] = 32'h01095020;  // add  $t2, $t0, $t1
+    mem[3] = 32'hAC0A0000;  // sw   $t2, 0($zero)
+    mem[4] = 32'h8C0B0000;  // lw   $t3, 0($zero)
+    mem[5] = 32'h012A5822;  // sub  $t3, $t1, $t2
+    integer i;
+    for (i = 6; i < 1024; i = i + 1) begin
+        mem[i] = 32'h00000000;  // nop
+    end
+end 
 
-/*
-  initial begin
-    //mem[0] = 32'hFFC4A303;
-    //mem[1] = 32'h00832383;
-    // mem[0] = 32'h0064A423;
-    // mem[1] = 32'h00B62423;
-    mem[0] = 32'h0062E233;
-    // mem[1] = 32'h00B62423;
-
-  end
-*/
 endmodule
