8cc1e99ccd6ba1c434371837f66a2b0b0e2a35cb
RudraThakar <b23ee1100@iitj.ac.in>
2025-09-10 23:05:21 +0530

    updated PC

--- PC.v
+++ PC.v
@@ -2,8 +2,7 @@
 module PC_Module(clk,rst,PC,PC_Next);
     input clk,rst;
     input [31:0]PC_Next;
-    output [31:0]PC;
-    reg [31:0]PC;
+    output reg [31:0]PC;
 
     always @(posedge clk)
     begin
