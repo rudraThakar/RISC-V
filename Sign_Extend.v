73c1338c65339fb3af8d410e5f69af1a82da3195
RudraThakar <b23ee1100@iitj.ac.in>
2025-09-10 23:06:45 +0530

    Updated Sign Extension

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -4,6 +4,6 @@ module Sign_Extend (In,ImmSrc,Imm_Ext);
     output [31:0] Imm_Ext;
 
     assign Imm_Ext =  (ImmSrc == 2'b00) ? {{20{In[31]}},In[31:20]} : 
-                     (ImmSrc == 2'b01) ? {{20{In[31]}},In[31:25],In[11:7]} : 32'h00000000; 
+                    (ImmSrc == 2'b01) ? {{20{In[31]}},In[31:25],In[11:7]} : 32'h00000000; 
 
 endmodule
