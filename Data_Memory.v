11faf66e4c153d0d243a698dae5b31a246d1bffb
rudraThakar <b23ee1100@iitj.ac.in>
2025-10-27 01:03:29 +0530

    Data Memory

--- Data_Memory.v
+++ Data_Memory.v
@@ -1,26 +1,39 @@
+module Data_Memory(
+    input clk,
+    input rst,
+    input WE,
+    input [31:0] A,
+    input [31:0] WD,
+    output reg [31:0] RD
+);
 
-module Data_Memory(clk,rst,WE,WD,A,RD);
-    input clk, rst, WE;
-    input [31:0] A, WD;
-    output [31:0] RD;
-
-    reg [31:0] data_mem [1023 : 0];
-
-//Logic for right shift is similar to that as in Instructuon memory
-
-    always @(posedge clk)
-        begin
-            if (WE)
-                data_mem[A>>2] <= WD;
-        end
-
-    assign RD = (rst == 1'b1) ? {32{1'b0}} : data_mem[A>>2]
+    reg [31:0] data_mem [0:1023];
 
     integer i;
-    inital begin
-        for(i=0; i<1024; i=i+1)
+
+    // Initialize memory to 0
+    initial begin
+        for (i = 0; i < 1024; i = i + 1)
             data_mem[i] = 32'b0;
     end
 
-endmodule
+    // Write operation on positive edge
+    always @(posedge clk) begin
+        if (rst) begin
+            for (i = 0; i < 1024; i = i + 1)
+                data_mem[i] <= 32'b0;
+        end
+        else if (WE) begin
+            data_mem[A >> 2] <= WD;   // A >> 2 because word addressing (32-bit aligned)
+        end
+    end
 
+    // Read operation (combinational)
+    always @(*) begin
+        if (rst)
+            RD = 32'b0;
+        else
+            RD = data_mem[A >> 2];
+    end
+
+endmodule
