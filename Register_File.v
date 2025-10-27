8c19a129d95947fab2cb4868c2f95082f976ee31
rudraThakar <b23ee1100@iitj.ac.in>
2025-10-28 01:51:04 +0530

    Register File

--- Register_File.v
+++ Register_File.v
@@ -1,32 +1,32 @@
+module Register_File(clk, rst, WE3, WD3, A1, A2, A3, RD1, RD2);
 
-module Register_File(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
-
-    input clk,rst,WE3;       //WE3 = Write Enable
-    input [4:0]A1,A2,A3;     //Address of the register
-    input [31:0]WD3;         //data to be written in the register
-    output [31:0]RD1,RD2;    //data read from the register
+    input clk, rst, WE3;           // WE3 = Write Enable
+    input [4:0] A1, A2, A3;        // Register addresses
+    input [31:0] WD3;              // Data to be written
+    output [31:0] RD1, RD2;        // Data read from registers
 
     reg [31:0] Register [31:0];
-
-//Register Write is Synchronous Sequential
-//Register Read is combinational
-
     integer i;
-    always @ (posedge clk or posedge rst)
-    begin
-        if(rst)
-            for (i=0; i<32; i=i+1){
-                Register[i] <= 0;
-            }
-        else
-            if(WE3 & (A3!=5'b00000))    //notreg[0] & write_enabled
-                Register[A3] <= WD3;
-    end
-    assign RD1 = (rst==1'b0) ? 32'd0 : Register[A1];
-    assign RD2 = (rst==1'b0) ? 32'd0 : Register[A2];
 
+    // Synchronous write, asynchronous reset
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            for (i = 0; i < 32; i = i + 1)
+                Register[i] <= 32'b0;
+        end
+        else if (WE3 && (A3 != 5'b00000)) begin
+            Register[A3] <= WD3;   // Write only if not register 0
+        end
+    end
+
+    // Combinational read
+    assign RD1 = (rst) ? 32'd0 : Register[A1];
+    assign RD2 = (rst) ? 32'd0 : Register[A2];
+
+    // Ensure $zero register always stays zero
     initial begin
-        Register[0] = 32'h00000000;     //$zero register is constant
+        Register[0] = 32'b0;
     end
 
 endmodule
+
